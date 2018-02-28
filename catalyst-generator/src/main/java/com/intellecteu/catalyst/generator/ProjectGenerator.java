fd1b8b7f628161b89359c284b03c778ef3d06253
Igor Ivaniuk <igor.ivaniuk@intellecteu.com>
2018-02-28 17:04:55 +0100

    BCCAT-30 FIltering out nodep dependencies from model

--- catalyst-generator/src/main/java/com/intellecteu/catalyst/generator/ProjectGenerator.java
+++ catalyst-generator/src/main/java/com/intellecteu/catalyst/generator/ProjectGenerator.java
@@ -136,7 +136,9 @@ public class ProjectGenerator {
 
   private static List<Dependency> filterDependencies(List<Dependency> dependencies,
       String scope) {
-    return dependencies.stream().filter(dep -> scope.equals(dep.getScope()))
+    return dependencies.stream().filter(
+        dep -> scope.equals(dep.getScope())
+            && !Dependency.CATEGORY_NO_DEPENDENCY.equals(dep.getCategory()))
         .sorted(DependencyComparator.INSTANCE)
         .collect(Collectors.toList());
   }
