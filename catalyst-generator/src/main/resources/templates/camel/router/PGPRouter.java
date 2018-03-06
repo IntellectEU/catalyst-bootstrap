b1708be1db6ecb4825495da119761c40d789761f
Igor Ivaniuk <igor.ivaniuk@intellecteu.com>
2018-03-06 15:36:13 +0100

    BCCAT-5 PGP route and proeprties

--- catalyst-generator/src/main/resources/templates/camel/router/PGPRouter.java
+++ catalyst-generator/src/main/resources/templates/camel/router/PGPRouter.java
@@ -4,10 +4,8 @@
 
 package <%packageName%>;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spi.IdempotentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,14 +14,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class PGPRouter extends RouteBuilder {
 
+  @Value("${catalyst.pgp.privateKeyFile}")
+  private String privateKeyFile;
+
+  @Value("${catalyst.pgp.privateKeyUserId}")
+  private String privateKeyUserId;
+
+  @Value("${catalyst.pgp.privateKeyPassword}")
+  private String privateKeyPassword;
+
+  @Value("${catalyst.pgp.publicKeyFile}")
+  private String publicKeyFile;
+
+  @Value("${catalyst.pgp.publicKeyUserId}")
+  private String publicKeyUserId;
+
 
   @Override
   public void configure() {
-    from("timer:name?period=1000")
-        .setHeader("messageId", simple("random(10)", String.class))
-        .log("Message identifier: ${header.messageId}")
-        .log("Processing duplicate message with id: ${header.messageId}")
-        .to("mock:duplicate")
+    from("timer:name?period=2000")
+        .setBody(simple("${bean:java.lang.System?method=currentTimeMillis}", String.class))
+        .log("Original message: ${body}")
+        .to("direct:encrypt");
+
+    from("direct:encrypt")
+        // Encrypt the message with public key
+        .marshal().pgp(publicKeyFile, publicKeyUserId)
+        .log("Encrypted message: ${bodyAs(java.lang.String)}")
+        .to("direct:decrypt");
+
+    from("direct:decrypt")
+        // Decrypt using private key and password (passphrase)
+        .unmarshal().pgp(privateKeyFile, privateKeyUserId, privateKeyPassword)
+        .log("Decrypted message: ${body}");
   }
 }
 
