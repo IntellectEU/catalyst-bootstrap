84bbe4cf8519645689c50ebff818377090aa7dd1
Igor Ivaniuk <igor.ivaniuk@intellecteu.com>
2018-03-19 16:40:43 +0100

    BCCAT-51 Removed @Primary

--- catalyst-generator/src/main/resources/templates/camel/config/Sql2LogConfig.java
+++ catalyst-generator/src/main/resources/templates/camel/config/Sql2LogConfig.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * Configuration of the datasource used for SQL endpoint
