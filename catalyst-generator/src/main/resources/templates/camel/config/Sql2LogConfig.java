86e0150538b09ea03b451625302359fa8c0b702f
Igor Ivaniuk <igor.ivaniuk@intellecteu.com>
2018-03-19 16:22:40 +0100

    BCCAT-51 SQL2Log config

---
+++ catalyst-generator/src/main/resources/templates/camel/config/Sql2LogConfig.java
@@ -0,0 +1,47 @@
+{{=<% %>=}}
+
+<%license%>
+
+package <%packageName%>;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.camel.component.sql.SqlComponent;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Configuration of the datasource used for SQL endpoint
+ */
+@Configuration
+public class Sql2LogConfig {
+
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource")
+  public DataSourceProperties dataSourceProperties() {
+    return new DataSourceProperties();
+  }
+
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource.hikari")
+  public HikariDataSource dataSource(DataSourceProperties dataSourceProperties) {
+    return (HikariDataSource) dataSourceProperties
+        .initializeDataSourceBuilder()
+        .type(HikariDataSource.class)
+        .build();
+  }
+
+  @Bean(name = "sql")
+  public SqlComponent sqlComponent(HikariDataSource dataSource) {
+    SqlComponent sqlComponent = new SqlComponent();
+    sqlComponent.setDataSource(dataSource);
+    return sqlComponent;
+  }
+
+}
+
+<%={{ }}=%>
