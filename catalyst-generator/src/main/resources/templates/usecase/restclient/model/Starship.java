9d26484e7986ea78997a973a12be35383816b3bf
tonya <antonina.kohut@intellecteu.com>
2018-07-19 13:39:03 +0300

    BCCAT-170 test sample is added to the rest client use case

--- catalyst-generator/src/main/resources/templates/usecase/restclient/model/Starship.java
+++ catalyst-generator/src/main/resources/templates/usecase/restclient/model/Starship.java
@@ -4,23 +4,29 @@
 
     package <%fullPackageName%>;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Starship model represents a single transport craft that has hyperdrive capability.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Starship implements Serializable {
 
+  @JsonProperty("name")
   public String name;
 
+  @JsonProperty("model")
   public String model;
 
+  @JsonProperty("starship_class")
   public String starshipClass;
 
-  public Starship(String name, String model, String starshipClass) {
-    this.name = name;
-    this.model = model;
-    this.starshipClass = starshipClass;
-  }
 }
 <%={{ }}=%>
